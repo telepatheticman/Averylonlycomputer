a785a402136dad3c4d5c837459b8757ec5d59675
Frc5572 <wwatso1tech@gmail.com>
2016-10-08 15:49:38 -0500

    COLORS

--- mazeSolver.cpp
+++ mazeSolver.cpp
@@ -4,6 +4,11 @@
 #include <vector>
 #include <string>
 #include <unistd.h>
+#include <cstdlib>
+#include <algorithm>
+#ifdef __unix__
+#include <sys/time.h>
+#endif
 
 using namespace std;
 
@@ -32,8 +37,49 @@ int findDudec(int r, int c, int d, vector<vector<vector<char> > > *maze);
 int findDuded(int r, int c, int d, vector<vector<vector<char> > > *maze);
 int findSpace(int r, int c, int d, vector<vector<vector<char> > > *maze, char rep);
 void showMaze(int r, int c, int d, vector<vector<vector<char> > > *maze, bool solution);
+void generate_maze(int x_dim, int y_dim, int z_dim, const char *filename);
 
 int main(void){
+#ifdef __unix__
+timespec ts;
+clock_gettime(CLOCK_REALTIME, &ts);
+srand(ts.tv_nsec);
+#define RST  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
+#define FRED(x) KRED x RST
+#define FGRN(x) KGRN x RST
+#define FYEL(x) KYEL x RST
+#define FBLU(x) KBLU x RST
+#define FMAG(x) KMAG x RST
+#define FCYN(x) KCYN x RST
+#define FWHT(x) KWHT x RST
+
+#define BOLD(x) "\x1B[1m" x RST
+#define UNDL(x) "\x1B[4m" x RST
+#endif
+    string generate = "";
+    bool gen = true;
+    while(true){
+    cout << "Generate New Map?[y/N] ";
+    getline(cin, generate);
+    if (generate == "y" || generate == "Y"){
+        gen = true;
+	break;
+    }else
+    if (generate == "N" || generate == "n"){
+        gen = false;
+	break;
+    }
+    cout << "could not parse neither 'y' nor 'n'" << endl;
+    }
+    if(gen) generate_maze(rand() % 5 + 10, rand() % 5 + 10, rand() % 10 + 5, "maze.txt");
     ifstream iFile ("maze.txt");
     string mazeLine = "";
     string input = "";
@@ -87,6 +133,25 @@ struct direction{
 
 direction direction;
 
+void draw(vector<vector<vector<char> > > *maze, int x, int z, int y, int moveup){
+#ifdef __unix__
+			if((*maze)[x][z][y + moveup] == solve || (*maze)[x][z][y + moveup] == Mazer) 
+				cout << BOLD(FBLU("#"));
+			else
+			if((*maze)[x][z][y + moveup] == up)
+				cout << BOLD(FGRN("#"));
+			else
+			if((*maze)[x][z][y + moveup] == down)
+				cout << BOLD(FRED("#"));
+			else
+			if((*maze)[x][z][y + moveup] == E)
+				cout << BOLD(FYEL("#"));
+			else  cout <<(*maze)[x][z][y + moveup];
+			#else
+			cout << (*maze)[x][z][y + moveup];
+			#endif
+}
+
 int solveMaze(vector<vector<vector<char> > > *maze, bool showStep){
     findSpace(x, y, z, maze, Mazer);
     findSpace(x, y, z, maze, E);
@@ -101,16 +166,18 @@ int solveMaze(vector<vector<vector<char> > > *maze, bool showStep){
     else{
         makeMove(r, c, d, maze);
         if (showStep) {
-            sleep(1);
+            usleep(1000);
             printStep(x, y, d, maze);
         }
     }
     solveMaze(maze, showStep);
 }
 
+int step = 0;
+
 void printStep(int r, int c, int d, vector<vector<vector<char> > > *maze){
     string line = "";
-    cout << " On Layer: " << d << endl;
+    cout << " On Layer: " << d << ", Step: " << (++step) << endl;
     for (int x = 0; x < r; x ++){
         for (int y = 0; y < c; y ++){
             cout << (*maze)[x][y][d];
@@ -404,7 +471,8 @@ void limitShow(int r, int c, int d, int lim, vector<vector<vector<char> > > *maz
                             (*maze)[x][z][y + moveup] = (*maze)[x][z][y + moveup];
                         }
                     }
-                    cout << (*maze)[x][z][y + moveup];
+			
+                draw(maze,x,z,y,moveup);
                 }
                 cout << " ";
             }
@@ -433,10 +501,122 @@ void limitShow(int r, int c, int d, int lim, vector<vector<vector<char> > > *maz
                         (*maze)[x][z][y + moveup] = (*maze)[x][z][y + moveup];
                     }
                 }
-                cout << (*maze)[x][z][y + moveup];
+                draw(maze,x,z,y,moveup);
             }
             cout << " ";
         }
         cout << endl;
     }
+}
+
+int rand_number(int low, int high){
+	return low + rand() % (high - low);
+}
+
+struct p3 {
+int x, y, z;
+p3(int x, int y, int z) : x(x), y(y), z(z) {}
+} typedef point3;
+
+void generate_maze(int x_dim, int y_dim, int z_dim, const char *filename){
+	vector<vector<vector<bool> > > data;
+	data.resize(x_dim);
+	for(int x = 0; x < x_dim; x++){
+		data[x].resize(y_dim);
+		for(int y = 0; y < y_dim; y++){
+			data[x][y].resize(z_dim);
+			for(int z = 0; z < z_dim; z++){
+				data[x][y][z] = true;
+			}
+		}
+	}
+
+	int randx = rand_number(1, x_dim - 1);
+	int randy = rand_number(1, y_dim - 1);
+	int randz = rand_number(0, z_dim);
+
+	vector<point3> edges;
+	cout << randx << "," << randy << "," << randz << endl;
+	data[randx][randy][randz] = false;
+	if(randx != 1) edges.push_back(point3(randx - 1, randy, randz));
+	if(randx != x_dim - 2) edges.push_back(point3(randx + 1, randy, randz));
+	if(randy != 1) edges.push_back(point3(randx, randy - 1, randz));
+	if(randy != y_dim - 2) edges.push_back(point3(randx, randy + 1, randz));
+	if(randz != 0) edges.push_back(point3(randx, randy, randz - 1));
+	if(randz != z_dim - 1) edges.push_back(point3(randx, randy, randz + 1));
+
+	/* Create Inner Maze */
+
+	while(!edges.empty()){
+		point3 curr = edges.back();
+		edges.pop_back();
+		int count = 0;
+		if(!data[curr.x + 1][curr.y][curr.z]) count++;
+		if(!data[curr.x - 1][curr.y][curr.z]) count++;
+		if(!data[curr.x][curr.y + 1][curr.z]) count++;
+		if(!data[curr.x][curr.y - 1][curr.z]) count++;
+		if(curr.z != z_dim - 1 && !data[curr.x][curr.y][curr.z + 1]) count++;
+		if(curr.z != 0 && !data[curr.x][curr.y][curr.z - 1]) count++;
+		if(count < 2){
+			data[curr.x][curr.y][curr.z] = false;
+			if(curr.x != 1 && data[curr.x - 1][curr.y][curr.z]) edges.push_back(point3(curr.x - 1, curr.y, curr.z));
+			if(curr.x != x_dim - 2 && data[curr.x + 1][curr.y][curr.z]) edges.push_back(point3(curr.x + 1, curr.y, curr.z));
+			if(curr.y != 1 && data[curr.x][curr.y - 1][curr.z]) edges.push_back(point3(curr.x, curr.y - 1, curr.z));
+			if(curr.y != y_dim - 2 && data[curr.x][curr.y + 1][curr.z]) edges.push_back(point3(curr.x, curr.y + 1, curr.z));
+			if(curr.z != 0 && data[curr.x][curr.y][curr.z - 1]) edges.push_back(point3(curr.x, curr.y, curr.z - 1));
+			if(curr.z != z_dim - 1 && data[curr.x][curr.y][curr.z + 1]) edges.push_back(point3(curr.x, curr.y, curr.z + 1));
+		}
+		random_shuffle(edges.begin(), edges.end());
+	}
+
+	/* Create Beginning and End */
+
+	bool beginning = false, end = false;
+	for(int i = 0; i < 2; i++)
+	while(true){
+		int rand_val = rand_number(0, 2*(x_dim + y_dim));
+		bool opposite = (rand_val - (x_dim+y_dim) >= 0);
+		bool x = (rand_val % (x_dim+y_dim))-x_dim < 0;
+		int val = (rand_val % (x_dim+y_dim)) - (x ? 0 : x_dim);
+		int layer = rand_number(0, z_dim);
+		int dx, dy;
+		if(x){
+			dx = val;
+			dy = (opposite ? y_dim - 1 : 0);
+			if(!data[dx][dy][layer]) continue;
+			if(opposite && !data[dx][dy - 1][layer]){
+				data[dx][dy][layer] = false;
+				break;
+			} else if(!opposite && !data[dx][dy + 1][layer]){
+				data[dx][dy][layer] = false;
+				break;
+			}
+		} else {
+			dx = (opposite ? x_dim - 1 : 0);
+			dy = val;
+			if(!data[dx][dy][layer]) continue;
+			if(opposite && !data[dx - 1][dy][layer]){
+				data[dx][dy][layer] = false;
+				break;
+			} else if(!opposite && !data[dx + 1][dy][layer]){
+				data[dx][dy][layer] = false;
+			}
+		}
+		
+	}
+
+
+	ofstream file;
+	file.open(filename);
+	file << y_dim << " " << x_dim << " " << z_dim << endl;
+	for(int z = 0; z < z_dim; z++){
+		for(int y = 0; y < y_dim; y++){
+			for(int x = 0; x < x_dim; x++){
+				file << (data[x][y][z] ? "#" : " ");
+			}
+			file << endl;
+		}
+		file << endl;
+	}
+	file.close();
 }
